97b43fb65d3554c3d4390aa28861343fa3cc1351
polymer13 <mipsmartin@gmail.com>
2024-01-21 03:54:18 +0000

    Formating code.

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,47 @@ extern crate cgmath;
 #[macro_use]
 extern crate cenum;
 
-
 mod core;
-mod resource;
 mod helpers;
+mod resource;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::io;
 use std::path::PathBuf;
 
-use crate::core::pipeline::Pipeline3D;
-use crate::core::pipeline::mgl::s3tc;
 pub use crate::core::app;
+use crate::core::pipeline::mgl::s3tc;
+use crate::core::pipeline::Pipeline3D;
 
-use cgmath::prelude::{ Matrix, SquareMatrix };
-use gl::types::*;
 use cgmath::prelude::*;
-
+use cgmath::prelude::{Matrix, SquareMatrix};
+use gl::types::*;
 
 type Matrix4 = cgmath::Matrix4<f32>;
 type Vector3 = cgmath::Vector3<f32>;
 
-extern "system" fn gl_error_cb (
-    _source : GLenum ,
-    err_type : GLenum ,
-    _id : GLuint ,
-    severity : GLenum ,
-    _length : GLsizei ,
+extern "system" fn gl_error_cb(
+    _source: GLenum,
+    err_type: GLenum,
+    _id: GLuint,
+    severity: GLenum,
+    _length: GLsizei,
     message: *const GLchar,
-    _user_param : *mut GLvoid
-)
-{
+    _user_param: *mut GLvoid,
+) {
     use std::ffi::CStr;
-    println!("GL ERROR CALLBACK: {}, type: 0x{}, severity = 0x{}, message = {}",
-             if err_type == gl::DEBUG_TYPE_ERROR { "** GL ERROR **"  } else { "" },
-             err_type, severity, unsafe {CStr::from_ptr(message).to_str().unwrap()}
+    println!(
+        "GL ERROR CALLBACK: {}, type: 0x{}, severity = 0x{}, message = {}",
+        if err_type == gl::DEBUG_TYPE_ERROR {
+            "** GL ERROR **"
+        } else {
+            ""
+        },
+        err_type,
+        severity,
+        unsafe { CStr::from_ptr(message).to_str().unwrap() }
     );
-
 }
 
 // TODO: Proposal of new system for managing objects within the scene
@@ -70,9 +73,7 @@ extern "system" fn gl_error_cb (
 // * Primary target in mind: Nalgebra *
 //
 
-
 fn process_args() -> app::Arguments {
-
     let mut cmd_args = std::env::args();
 
     let mut args = app::Arguments {
@@ -85,7 +86,7 @@ fn process_args() -> app::Arguments {
             "-d" | "--game-dir" => {
                 if let Some(path) = cmd_args.next() {
                     let p = PathBuf::from(path);
-                    if ! p.exists() {
+                    if !p.exists() {
                         panic!("Path specified for game directory does not exist!");
                     }
                     args.game_dir = Some(p);
@@ -105,8 +106,7 @@ fn process_args() -> app::Arguments {
     args
 }
 
-fn main () -> io::Result<()> {
-
+fn main() -> io::Result<()> {
     use std::clone::Clone;
 
     let app_args = process_args();
@@ -114,29 +114,34 @@ fn main () -> io::Result<()> {
     let app_cfg = app::AppConfig {
         args: app_args.clone(),
         window_size: (1024, 768),
-        window_title: ("darkest v0.1.0".to_owned())
+        window_title: ("darkest v0.1.0".to_owned()),
     };
 
     let mut app = app::AppCore::init(app_cfg).unwrap();
 
     use crate::core::pipeline::InitError;
-    let mut p3d = Pipeline3D::create_and_prepare(&app).map_err(|e| {
-        match e {
-            InitError::FailedLoadingResource(resource::BufferLoaderError::IoError { file_path, io_error })
-                if io_error.kind() == io::ErrorKind::NotFound => {
-                    panic!(format!("Resource not found required by core pipeline: {:?}",
-                                   file_path.map_or("Path not specified by error".to_owned(), |a| a.display().to_string())))
-                },
+    let mut p3d = Pipeline3D::create_and_prepare(&app)
+        .map_err(|e| match e {
+            InitError::FailedLoadingResource(resource::BufferLoaderError::IoError {
+                file_path,
+                io_error,
+            }) if io_error.kind() == io::ErrorKind::NotFound => {
+                panic!(
+                    "Resource not found required by core pipeline: {:?}",
+                    file_path.map_or("Path not specified by error".to_owned(), |a| a
+                        .display()
+                        .to_string())
+                )
+            }
             InitError::ShaderIssue(issue) => {
-                panic!(format!("Core shader issue: {}", issue))
+                panic!("Core shader issue: {}", issue)
             }
             _ => {}
-        }
-    }).unwrap();
+        })
+        .unwrap();
 
     // Setup debug messaging
     unsafe {
-
         match_bitfield! {
             match ( app_args.print_errors ) {
                 app::ErrorGroups::GL_ERRORS => {
@@ -151,21 +156,24 @@ fn main () -> io::Result<()> {
     }
 
     let model_ids = {
-
-        let cube = helpers::mesh3d::load_obj(&app, "./assets/cube.obj").pop().unwrap();
+        let cube = helpers::mesh3d::load_obj(&app, "./assets/cube.obj")
+            .pop()
+            .unwrap();
         p3d.activate_shader();
 
-        let susane = helpers::mesh3d::load_obj(&app, "./assets/susane.obj").pop().unwrap();
+        let susane = helpers::mesh3d::load_obj(&app, "./assets/susane.obj")
+            .pop()
+            .unwrap();
         p3d.activate_shader();
 
-        let light_maps = helpers::mesh3d::load_dds_normal_mapped_lightmaps (
-            &app, "assets/diff.dds", "assets/spec.dds", "assets/norm.dds"
+        let light_maps = helpers::mesh3d::load_dds_normal_mapped_lightmaps(
+            &app,
+            "assets/diff.dds",
+            "assets/spec.dds",
+            "assets/norm.dds",
         );
 
-         p3d.prepare_normal_mapped_textured_meshes(&[
-            ( &light_maps, &cube ),
-            ( &light_maps, &susane )
-        ])
+        p3d.prepare_normal_mapped_textured_meshes(&[(&light_maps, &cube), (&light_maps, &susane)])
     };
 
     let cube_id = model_ids[0].clone();
@@ -184,88 +192,88 @@ fn main () -> io::Result<()> {
     let mut enable_blinn = false;
 
     'main_loop: loop {
-
         let time = timer.elapsed();
 
-
         for event in app.sdl_event_pump.poll_iter() {
-
             match event {
-                Event::Quit {..} => break 'main_loop,
-                Event::KeyDown { keycode, ..} => {
+                Event::Quit { .. } => break 'main_loop,
+                Event::KeyDown { keycode, .. } => {
                     if let Some(k) = keycode {
                         match k {
-                            Keycode::Escape => {break 'main_loop;},
-                            Keycode::R => {
-                            },
+                            Keycode::Escape => {
+                                break 'main_loop;
+                            }
+                            Keycode::R => {}
                             Keycode::B => {
                                 if enable_blinn {
-                                    unsafe {gl::Uniform1i(core::pipeline::gpu::attrs::USE_BLINN_FLAG, 0);}
+                                    unsafe {
+                                        gl::Uniform1i(
+                                            core::pipeline::gpu::attrs::USE_BLINN_FLAG,
+                                            0,
+                                        );
+                                    }
                                     enable_blinn = false;
                                 } else {
-                                    unsafe {gl::Uniform1i(core::pipeline::gpu::attrs::USE_BLINN_FLAG, 1);}
+                                    unsafe {
+                                        gl::Uniform1i(
+                                            core::pipeline::gpu::attrs::USE_BLINN_FLAG,
+                                            1,
+                                        );
+                                    }
                                     enable_blinn = true;
                                 }
-                            },
+                            }
 
-                            _ => {},
+                            _ => {}
                         }
                     }
+                }
+
+                Event::MouseMotion {
+                    mousestate: _mstate,
+                    x: _x,
+                    y: _y,
+                    xrel: _xrel,
+                    yrel: _yrel,
+                    ..
+                } => {}
+
+                Event::MouseButtonDown { mouse_btn, .. } => match mouse_btn {
+                    sdl2::mouse::MouseButton::Left => {}
+                    _ => {}
                 },
 
-                Event::MouseMotion { mousestate : _mstate, x : _x, y : _y, xrel : _xrel, yrel : _yrel, .. } => {
-
-                },
-
-                Event::MouseButtonDown {mouse_btn, ..} => {
-                    match mouse_btn {
-                        sdl2::mouse::MouseButton::Left => {
-                        }
-                        _ => {}
-                    }
-                },
-
-                Event::MouseButtonUp {mouse_btn, ..} => {
-                    match mouse_btn {
-                        sdl2::mouse::MouseButton::Left => {
-                        },
-                        _ => {}
-                    }
+                Event::MouseButtonUp { mouse_btn, .. } => match mouse_btn {
+                    sdl2::mouse::MouseButton::Left => {}
+                    _ => {}
                 },
 
                 _ => {}
             }
         }
 
-        let model_scale = Matrix4::from_scale(
-            0.5f32
-        );
+        let model_scale = Matrix4::from_scale(0.5f32);
 
-        let model_mat  = model_scale * cgmath::Matrix4::<f32>::from_translation(
-            cgmath::Vector3::new( 1.1, 0.0, 0.0 ),
-        );
+        let model_mat = model_scale
+            * cgmath::Matrix4::<f32>::from_translation(cgmath::Vector3::new(1.1, 0.0, 0.0));
 
-        let model2_mat  = model_scale * cgmath::Matrix4::<f32>::from_translation(
-            cgmath::Vector3::new( -1.1, 0.0, 0.0 ),
-        );
+        let model2_mat = model_scale
+            * cgmath::Matrix4::<f32>::from_translation(cgmath::Vector3::new(-1.1, 0.0, 0.0));
 
         let t = time.as_millis() as f32 / 1000.0;
         let camera_dist = 3.0;
-        let camera_pos = cgmath::Point3::<f32>::new(
-            camera_dist * t.sin(),
-            camera_dist * t.sin() - t.cos(),
-            camera_dist *  t.cos()
-        );
-         
-        let view_center = cgmath::Point3::new( 0.0, 0.0, 0.0 );
+        let camera_pos = cgmath::Point3::<f32>::new(camera_dist, camera_dist, camera_dist);
+
+        let view_center = cgmath::Point3::new(0.0, 0.0, 0.0);
 
         // let view_mat = Matrix4::from_translation(
         //     Vector3::new(1.0, 1.0, 5.0)
         // ).invert().unwrap();
 
-        let view_mat = cgmath::Matrix4::<f32>::look_at (
-            camera_pos, view_center,
-            cgmath::Vector3::new( 0.0, 1.0, 0.0 ),
+        let view_mat = cgmath::Matrix4::<f32>::look_at(
+            camera_pos,
+            view_center,
+            cgmath::Vector3::new(0.0, 1.0, 0.0),
         );
 
         // let model_view_mat = view_mat * model_mat;
@@ -275,9 +283,7 @@ fn main () -> io::Result<()> {
         let normal2_mat = model2_mat.invert().unwrap().transpose();
         // let normal_mat = model_view_mat.invert().unwrap().transpose();
 
-        let proj_mat  = cgmath::perspective(
-            cgmath::Deg(75.0), 4.0/3.0, 0.1, 1000.0
-        );
+        let proj_mat = cgmath::perspective(cgmath::Deg(75.0), 4.0 / 3.0, 0.1, 1000.0);
 
         let _mvp = proj_mat * view_mat * model_mat;
 
@@ -287,6 +293,7 @@ fn main () -> io::Result<()> {
             gl::Uniform3fv(10, 1, camera_pos.as_ptr());
         }
 
+        p3d.update_view_pos(camera_pos);
         p3d.update_projection_matrix(proj_mat);
         p3d.update_view_matrix(view_mat);
 
@@ -303,11 +310,12 @@ fn main () -> io::Result<()> {
         let time_end = timer.elapsed();
         let frame_duration = time_end - time;
 
-        let max_frame_duration = std::time::Duration::from_millis(1000/60);
+        let max_frame_duration = std::time::Duration::from_millis(1000 / 60);
 
         std::thread::sleep(
-            max_frame_duration.checked_sub(frame_duration)
-                              .unwrap_or(std::time::Duration::new(0,0))
+            max_frame_duration
+                .checked_sub(frame_duration)
+                .unwrap_or(std::time::Duration::new(0, 0)),
         );
     }
 
